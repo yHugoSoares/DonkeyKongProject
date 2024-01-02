dc85871b5bbb1f3e2f616da381337293b4419ade
Hugo Soares <a107293@alunos.uminho.pt>
2024-01-02 15:25:10 +0000

    Added Main file with all properties and map/object

---
+++ main.hs
@@ -0,0 +1,45 @@
+module Gloss where
+
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+
+type Estado = (Float, Float)
+
+type EstadoGloss = (Estado, Picture)
+
+estadoInicial :: Estado
+estadoInicial = (0,0)
+
+estadoGlossInicial :: Picture -> EstadoGloss 
+estadoGlossInicial z = (estadoInicial, z)
+
+reageEventoGloss :: Event -> EstadoGloss -> EstadoGloss 
+reageEventoGloss (EventKey (SpecialKey KeyUp) Down _ _)        ((x,y), z) = ((x,y+5), z)
+reageEventoGloss (EventKey (SpecialKey KeyDown) Down _ _)      ((x,y), z) = ((x,y-5), z)
+reageEventoGloss (EventKey (SpecialKey KeyLeft) Down _ _)      ((x,y), z) = ((x-5,y), z)
+reageEventoGloss (EventKey (SpecialKey KeyRight) Down _ _)     ((x,y), z) = ((x+5,y), z)
+reageEventoGloss _ s = s -- ignora qualquer outro evento
+
+reageTempoGloss :: Float -> EstadoGloss -> EstadoGloss 
+reageTempoGloss n ((x,y), z) = ((x,y), z)
+
+fr :: Int
+fr = 60
+
+dm :: Display
+dm = FullScreen
+-- dm - InWindow "Novo Jogo" (400, 400) (0, 0)
+
+desenhaEstadoGloss :: EstadoGloss -> Picture 
+desenhaEstadoGloss ((x,y), z) = Translate x y z
+
+main :: IO ()
+main = do
+    mario <- loadBMP "/home/hugo/Documents/LEI 1ano/git/images/8Bit_Mario.bmp"
+    play dm                             -- janela onde esta a decorrer
+        black                           -- cor do fundo da janela
+        fr                              -- framerate
+        (estadoGlossInicial (scale 0.15 0.15 mario))      -- estado inicial
+        desenhaEstadoGloss              -- desenha o estado do jogo
+        reageEventoGloss                -- reage a um evento
+        reageTempoGloss                 -- reage ao passar do tempo
---
+++ mapa.hs
@@ -0,0 +1,36 @@
+module Mapa where
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Data.Bitmap
+--import Data.Struct
+--import Maps
+
+--Datas do jogo
+
+data Mapa = Mapa (Posicao, Direcao) Posicao [[Bloco]]
+data Bloco = Escada | Plataforma | Alcapao | Vazio
+data Direcao = Norte | Sul | Leste | Oeste deriving Show
+type Posicao = (Double,Double)--par ordenado que define (coluna,linha)
+data Personagem = Personagem{ 
+velocidade :: Velocidade    --Um vetor de velocidade normal
+, tipo :: Entidade          --Indica se é um inimigo (e qual) ou o jogador
+, posicao :: Posicao        --Coordenadas atuais
+, direcao :: Direcao        --Para qual posiçao (N,S,L,O) está virado
+, tamanho :: (Double, Double)--Tamanho do personagem descrito por um rectângulo largura X altura
+, emEscada :: Bool          --Indica se o personagem se encontra a subir/descer uma escada
+, ressalta :: Bool          --Indica se o personagem deve trocar de direçao ao colidir com uma parede ou ao chegar no final de uma plataforma
+, vida :: Int               --Quantas vidas faltam
+, pontos :: Int             --Pontuaçao acumulada
+, aplicaDano :: (Bool, Double)--Indica se tem o martelo armado e por quanto tempo ainda. Quando ativo, cria uma hitbox à frente do jogador (com as dimensoes do jogador) e qualquer inimigo que encoste sofre dano
+}
+type Velocidade = Double 
+data Entidade = MacacoMalvado | Fantasma | Jogador
+
+data Colecionavel = Martelo | Moeda
+
+data Jogo = Jogo {
+mapa :: Mapa
+, inimigos :: [Personagem]
+, colecionaveis :: [(Colecionavel, Posicao)]
+, jogador :: Personagem
+}
