8849feaefe6f409f0587ca63dfd5387ac6ee1016
Hugo Soares <a107293@alunos.uminho.pt>
2024-01-11 16:00:41 +0000

    main.hs modified for some features

--- main.hs
+++ main.hs
@@ -7,28 +7,30 @@ import DataStruct
 
 type Estado = (Float, Float)
 
-type EstadoGloss = (Estado, Picture)
+type EstadoGloss = (Estado, Bool, Picture)
 
-mov = 20 -- Velocidade do movimento do Mário
+mov :: Float
+mov = 5  -- Velocidade do movimento do Mário
+jump :: Float
 jump = 40 -- valor de salto do Mário
 
 estadoInicial :: Estado
-estadoInicial = (0,0)
+estadoInicial = (0, 0)
 
-estadoGlossInicial :: Picture -> EstadoGloss 
-estadoGlossInicial z = (estadoInicial, z)
+estadoGlossInicial :: Picture -> EstadoGloss
+estadoGlossInicial z = (estadoInicial, False, z)
 
-reageEventoGloss :: Event -> EstadoGloss -> EstadoGloss 
-reageEventoGloss (EventKey (SpecialKey KeyUp) Down _ _)        ((x,y), z) = ((x,y+mov), z)
-reageEventoGloss (EventKey (SpecialKey KeyDown) Down _ _)      ((x,y), z) = ((x,y-mov), z)
-reageEventoGloss (EventKey (SpecialKey KeyLeft) Down _ _)      ((x,y), z) = ((x-mov,y), z)
-reageEventoGloss (EventKey (SpecialKey KeyRight) Down _ _)     ((x,y), z) = ((x+mov,y), z)
-reageEventoGloss (EventKey (SpecialKey KeySpace) Down _ _)     ((x,y), z) = ((x,y+jump), z)
-reageEventoGloss (EventKey (SpecialKey KeySpace) Up _ _)     ((x,y), z) = ((x,y-jump), z)
+reageEventoGloss :: Event -> EstadoGloss -> EstadoGloss
+reageEventoGloss (EventKey (SpecialKey KeyUp) Down _ _) ((x, y), _, z) = ((x, y + mov), True, z)
+reageEventoGloss (EventKey (SpecialKey KeyDown) Down _ _) ((x, y), _, z) = ((x, y - mov), True, z)
+reageEventoGloss (EventKey (SpecialKey KeyLeft) Down _ _) ((x, y), _, z) = ((x - mov, y), True, z)
+reageEventoGloss (EventKey (SpecialKey KeyRight) Down _ _) ((x, y), _, z) = ((x + mov, y), True, z)
+reageEventoGloss (EventKey (SpecialKey KeySpace) Down _ _) ((x, y), _, z) = ((x, y + jump), True, z)
+reageEventoGloss (EventKey (SpecialKey KeySpace) Up _ _) ((x, y), _, z) = ((x, y - jump), True, z)
 reageEventoGloss _ s = s -- ignora qualquer outro evento
 
-reageTempoGloss :: Float -> EstadoGloss -> EstadoGloss 
-reageTempoGloss n ((x,y), z) = ((x,y), z)
+reageTempoGloss :: Float -> EstadoGloss -> EstadoGloss
+reageTempoGloss _ ((x, y), _, z) = ((x, y), False, z)
 
 fr :: Int
 fr = 60
@@ -37,8 +39,8 @@ dm :: Display
 dm = FullScreen
 -- dm - InWindow "Novo Jogo" (400, 400) (0, 0)
 
-desenhaEstadoGloss :: EstadoGloss -> Picture 
-desenhaEstadoGloss ((x,y), z) = Translate x y z
+desenhaEstadoGloss :: EstadoGloss -> Picture
+desenhaEstadoGloss ((x, y), _, z) = Translate x y z
 
 main :: IO ()
 main = do
