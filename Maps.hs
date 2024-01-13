76fcda90d3055e6aa783751276bf81308d0034fa
Hugo Soares <a107293@alunos.uminho.pt>
2024-01-13 21:01:51 +0000

    Documentacao acabada

--- Maps.hs
+++ Maps.hs
@@ -1,3 +1,4 @@
+-- | Módulo contendo funções relacionadas ao desenho do mapa e personagens.
 module Maps where
 
 import DataStruct
@@ -6,53 +7,67 @@ import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Data.Bitmap
 import Data.Maybe
 
-type Jogador = (Entidade,[Picture])
-type Inimigo = [(Entidade,Picture)]
+-- | Tipo representando um jogador com sua aparência.
+type Jogador = (Entidade, [Picture])
+
+-- | Tipo representando um inimigo com sua aparência.
+type Inimigo = [(Entidade, Picture)]
+
+-- | Tipo representando uma lista de imagens.
 type PictureM = [Picture]
 
+-- | Função que desenha o jogador no ambiente.
 desenhaJogador :: Personagem -> Jogador -> Picture
 desenhaJogador (Personagem _ _ (i,j) d _ _ _ _ _ _) skin = desenhoJogador i j d skin
 
-desenhoJogador :: Float -> Float -> Direcao -> Jogador-> Picture
-desenhoJogador i j d (_,[marioO,marioL]) | d == Oeste = Translate i j  marioO
-                                         | d == Leste = Translate i j  marioL
-                                         |otherwise = Translate i j marioO
+-- | Função que desenha o jogador no ambiente de acordo com sua aparência.
+desenhoJogador :: Float -> Float -> Direcao -> Jogador -> Picture
+desenhoJogador i j d (_,[marioO,marioL])
+  | d == Oeste = Translate i j marioO
+  | d == Leste = Translate i j marioL
+  | otherwise = Translate i j marioO
 
+-- | Função que desenha os inimigos no ambiente.
 desenhaInimigo :: [Personagem] -> Inimigo -> [Picture]
-desenhaInimigo ((Personagem _ _ _ _ _ _ _ 0 _ _):t) skins        = desenhaInimigo t skins 
-desenhaInimigo ((Personagem _ tipo (i,j) _ _ _ _ _ _ _):t) skins = (desenhoInimigo i j tipo skins):desenhaInimigo t skins
-desenhaInimigo _ _                                               = []
+desenhaInimigo ((Personagem _ _ _ _ _ _ _ 0 _ _):t) skins = desenhaInimigo t skins
+desenhaInimigo ((Personagem _ tipo (i,j) _ _ _ _ _ _ _):t) skins = desenhoInimigo i j tipo skins : desenhaInimigo t skins
+desenhaInimigo _ _ = []
 
+-- | Função que desenha um inimigo no ambiente de acordo com sua aparência.
 desenhoInimigo :: Float -> Float -> Entidade -> Inimigo -> Picture
-desenhoInimigo i j tipo malvados = (Translate i j) image
-        where image = (fromJust . lookup tipo) malvados
+desenhoInimigo i j tipo malvados = Translate i j image
+  where image = fromJust (lookup tipo malvados)
 
+-- | Função que converte um bloco com sua aparência para uma imagem.
+pieceToPic :: (Bloco, [Picture]) -> Picture
+pieceToPic (Plataforma, pics) = pics !! 0
+pieceToPic (Escada, pics) = pics !! 1
+pieceToPic (Alcapao, pics) = pics !! 2
+pieceToPic (Vazio, pics) = Blank
 
-pieceToPic :: (Bloco,[Picture]) -> Picture
-pieceToPic (Plataforma,pics) = pics !! 0
-pieceToPic (Escada,pics) = pics !! 1
-pieceToPic (Alcapao,pics) = pics !! 2
-pieceToPic (Vazio,pics) = Blank    
+-- | Função que desenha uma linha do mapa.
+drawLine :: ([Bloco], [Picture]) -> Int -> [Picture]
+drawLine ([], _) _ = []
+drawLine (h:t, pics) x = Translate (fromIntegral (x * 16)) 0 (pieceToPic (h, pics)) : drawLine (t, pics) (x + 1)
 
-drawLine :: ([Bloco],[Picture]) -> Int -> [Picture]
-drawLine ([],_) _ = []
-drawLine ((h:t),pics) x = (Translate (fromIntegral(x*16)) 0 (pieceToPic (h,pics))):(drawLine (t,pics) (x+1))
+-- | Função que desenha o mapa.
+drawMap :: (Mapa, [Picture]) -> (Int, Int) -> [Picture]
+drawMap (Mapa _ _ [], _) _ = []
+drawMap (Mapa a b (h:t), pics) (x, y) = Translate 0 (fromIntegral (y * (-16))) (Pictures (drawLine (h, pics) x)) : drawMap (Mapa a b t, pics) (x, y + 1)
 
-drawMap :: (Mapa,[Picture]) -> (Int,Int) -> [Picture]
-drawMap (Mapa _ _ [],_) _ = []
-drawMap (Mapa a b (h:t),pics) (x,y) = (Translate 0 (fromIntegral(y*(-16))) (Pictures (drawLine (h,pics) x))):(drawMap (Mapa a b t,pics) (x,y+1))
-
+-- | Função que carrega as imagens necessárias para o jogo.
 carregaImagens :: IO [Picture]
-carregaImagens = do plataforma <- loadBMP "./img/Platform.bmp"
-                    escada <- loadBMP "./img/Ladder.bmp"
-                    alcapao <- loadBMP "./img/Ladder.bmp"
-                    return ([plataforma,escada,alcapao])  
+carregaImagens = do
+  plataforma <- loadBMP "./img/Platform.bmp"
+  escada <- loadBMP "./img/Ladder.bmp"
+  alcapao <- loadBMP "./img/Ladder.bmp"
+  return [plataforma, escada, alcapao]
 
+-- | Modo de exibição da janela do jogo.
 displayMode :: Display
-displayMode = InWindow "Game" (640,640) (0,0)
+displayMode = InWindow "Game" (640, 640) (0, 0)
 
-
-
+-- | Mapa utilizado no jogo.
 mapaGrande :: Mapa
 mapaGrande =  Mapa ((20,-200),Oeste) (0,0)
               [[Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma],
@@ -71,12 +86,14 @@ mapaGrande =  Mapa ((20,-200),Oeste) (0,0)
                [Plataforma,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Escada,Vazio,Vazio,Vazio,Plataforma],
                [Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma,Plataforma]]
 
-desenhaMapa :: Jogo -> [Picture] -> Jogador -> Inimigo -> Picture
-desenhaMapa (Jogo menu mapa mal colec jog) pics skin inimigo = translate (-120) (104) $ pictures (drawMap (mapa,pics) (0,0) ++ [desenhaJogador jog skin] ++ desenhaInimigo mal inimigo )
-
-
+-- | Personagem principal do jogo.
 mario :: Personagem
-mario = Personagem mov Jogador (40,-180) Oeste (16,16) False False 1 0 (False,0)
+mario = Personagem mov Jogador (40, -180) Oeste (16, 16) False False 1 0 (False, 0)
 
+-- | Lista de inimigos no jogo.
 malvados :: [Personagem]
-malvados = [(Personagem 1 MacacoMalvado (100,26) Oeste (39,32) False True 1 0 (False,0)),(Personagem 1 Fantasma (100,(-204)) Oeste (16,16) False True 1 0 (False,0)),(Personagem 1 Fantasma (160,(-204)) Oeste (16,16) False True 1 0 (False,0))]
+malvados =
+  [ Personagem 1 MacacoMalvado (100, 26) Oeste (39, 32) False True 1 0 (False, 0),
+    Personagem 1 Fantasma (100, -204) Oeste (16, 16) False True 1 0 (False, 0),
+    Personagem 1 Fantasma (160, -204) Oeste (16, 16) False True 1 0 (False, 0)
+  ]
