91bdb9f906309b35566c14f33bb5680a2717dd21
Hugo Soares <a107293@alunos.uminho.pt>
2024-01-13 20:56:56 +0000

    Documentacao acabada

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+-- | Módulo contendo a lógica principal do jogo e apresentação usando Gloss.
 module Main where
 
 import Graphics.Gloss
@@ -16,82 +17,74 @@ import Maps
 import Tarefa1
 -- import Tarefa3
 
--- Frame rate for the game
+-- | Taxa de quadros por segundo do jogo.
 fr :: Int
 fr = 50
 
--- Display settings
+-- | Configurações de exibição para a janela do jogo.
 dm :: Display
 dm = FullScreen
 -- dm = InWindow "Donkey Kong" (640, 640) (0, 0)
 
--- Type alias for the game state
+-- | Alias de tipo para o estado do jogo.
 type EstadoGloss = (Jogo, [Key], [Picture], Jogador, Inimigo)
 
--- Initial game state
+-- | Estado inicial do jogo.
 estadoInicial :: Jogo
 estadoInicial = Jogo (Opcoes Jogar) mapaGrande malvados [] mario
 
--- Function to create the initial state for Gloss, which includes a list of pictures
+-- | Função para criar o estado inicial para o Gloss, que inclui uma lista de imagens.
 estadoGlossInicial :: [Picture] -> Jogador -> Inimigo -> EstadoGloss
-estadoGlossInicial z skin inimigo = (estadoInicial,[], z, skin, inimigo)
+estadoGlossInicial z skin inimigo = (estadoInicial,[], z,skin, inimigo)
 
--- Function to handle events in Gloss
+-- | Função para lidar com eventos no Gloss.
 reageEventoGloss :: Event -> EstadoGloss -> EstadoGloss
 reageEventoGloss evento@(EventKey k Down _ _) (jogo@(Jogo (Opcoes _) m mal b jo) ,keys, z,skin, inimigo) = (stageMenu k jogo, reactKey evento keys, z,skin, inimigo)
                                                                                              where op = menu estadoInicial
 reageEventoGloss evento (estadoInicial,keys, z,skin, inimigo) = (estadoInicial, reactKey evento keys, z,skin, inimigo)
 
--- Function to update the game state
+-- | Função para atualizar o estado do jogo.
 atualizaEstado :: Float -> EstadoGloss -> EstadoGloss
 atualizaEstado n (estadoInicial@(Jogo _ mapa _ _ mario), keys, z, skin, inimigo) = (aplicaListaKey keys estadoInicial b, keys, z, skin, inimigo)
     where b = colisoesChao mapa mario || colisoesParede mapa mario
 
--- atualizaEstado :: Float -> EstadoGloss -> EstadoGloss
--- atualizaEstado n (estadoInicial@(Jogo _ mapa malvados _ mario), keys, z, skin, inimigo) =
---   (aplicaListaKey keys estadoAtualizado colisao, keys, z, skin, inimigo)
---     where
---       estadoAtualizado = Jogo ModoJogo mapa malvados novoB marioAtualizado
---       novoB = atualizaCollectibles n b  -- Assuming `atualizaCollectibles` updates the collectibles based on time
---       colisao = colisoesChao mapa marioAtualizado || colisoesParede mapa marioAtualizado
---       marioAtualizado = atualizaPersonagem n mario
---       b = colecionaveis estadoInicial  -- Assuming `coletaveis` is a function that gets the current list of collectibles
-
--- Função que atualiza o estado do jogo com base na tecla pressionada e no estado atual do jogo
+-- | Função que atualiza o estado do jogo com base na tecla pressionada e no estado atual do jogo.
 stageMenu :: Key -> Jogo -> Jogo
 stageMenu k jogo = case menu jogo of
         (Opcoes op) ->
             case k of
                 (SpecialKey KeyEnter) -> case op of
-                                            Jogar -> jogo {menu=ModoJogo}   -- Muda para o modo de jogo normal quando a tecla enter for pressionada
+                                            Jogar -> jogo {menu=ModoJogo}   -- Muda para o modo de jogo normal quando a tecla enter é pressionada
                                             Sair -> error "Fim do jogo"
                 (SpecialKey KeyDown) -> jogo {menu = Opcoes (mudaOP op)}  -- Move a seleção para a opção seguinte
                 (SpecialKey KeyUp)   -> jogo {menu = Opcoes (mudaOP op)}  -- Move a seleção para a opção anterior
                 _ -> jogo
         _ -> jogo
 
--- Função que muda a opção selecionada no menu
+-- | Função que muda a opção selecionada no menu.
 mudaOP :: Opcao -> Opcao
 mudaOP op = case op of
                Jogar -> Sair
                Sair -> Jogar
 
--- Função que desenha o estado atual do jogo na tela usando a biblioteca Gloss
+-- | Função que desenha o estado atual do jogo na tela usando a biblioteca Gloss.
 desenhaEstadoGloss :: EstadoGloss -> Picture
 desenhaEstadoGloss (Jogo (Opcoes op) _ _ _ _,_,_,_,_)  = drawOptions op
 desenhaEstadoGloss (estadoinicio@(Jogo ModoJogo _ _ _ _),keys, z,skin, inimigo) = desenhaMapa estadoinicio z skin inimigo
 
--- Função que desenha as opções do menu
-drawOptions op =   case op of
+-- | Função que desenha as opções do menu.
+drawOptions :: Opcao -> Picture
+drawOptions op = case op of
     Jogar -> Pictures [Translate (-50) 10 $ Color blue $ drawOption "Jogar",
                        Translate (-50) (-70) $ Color white $ drawOption "Sair"]
     Sair ->  Pictures [Color white $ Translate (-50) 10 $ drawOption "Jogar",
                        Color blue $ Translate (-50) (-70) $ drawOption "Sair"]
 
--- Função que desenha uma opção do menu
+-- | Função que desenha uma opção do menu.
+drawOption :: String -> Picture
 drawOption option =  Scale 0.5 0.5 $ Text option
 
--- Função principal que inicia o jogo
+-- | Função principal que inicia o jogo.
 main :: IO ()
 main = do
     loadMAPA <- carregaImagens
@@ -101,8 +94,8 @@ main = do
     fantasma <- loadBMP "./img/fantasma.bmp"
     play dm                             -- Inicia a janela do jogo
         black                           -- Cor do fundo da janela
-        fr                              -- Framerate
+        fr                              -- Taxa de quadros por segundo
         (estadoGlossInicial loadMAPA (Jogador,[marioOeste,marioLeste]) [(MacacoMalvado,macaco),(Fantasma, fantasma)])    -- Estado inicial do jogo
         desenhaEstadoGloss              -- Função que desenha o estado do jogo
         reageEventoGloss                -- Função que reage a eventos do usuário
-        atualizaEstado                  -- Função que atualiza o estado do jogo a cada frame
+        atualizaEstado                  -- Função que atualiza o estado do jogo a cada quadro
--- Tarefa1.hs
+++ Tarefa1.hs
@@ -1,45 +1,60 @@
 module Tarefa1 where
+
 import DataStruct
 import Maps
 
-colisoesParede :: Mapa -> Personagem -> Bool
-colisoesParede (Mapa _ _ mapa) Personagem {posicao = (x,y), direcao = dir} =
-    case dir of 
-        Oeste -> colisaoPlataforma (round ((x-16)/16),round (y/16))
-        Leste -> colisaoPlataforma (round ((x+16)/16),round (y/16))
-        _ ->  False
-    where 
-        colisaoPlataforma coord = 
-            case obterValor mapa coord of
-                Plataforma -> True 
-                Alcapao -> True
-                Vazio -> False
-                Escada -> False
+-- | Verifica colisões com paredes na direção especificada.
+colisoesParede :: Mapa          -- ^ Mapa atual do jogo.
+               -> Personagem    -- ^ Personagem do jogador.
+               -> Bool          -- ^ Verdadeiro se houver colisão, Falso caso contrário.
+colisoesParede (Mapa _ _ mapa) Personagem {posicao = (x, y), direcao = dir} =
+    case dir of
+        Oeste -> colisaoPlataforma (round ((x - 16) / 16), round (y / 16))
+        Leste -> colisaoPlataforma (round ((x + 16) / 16), round (y / 16))
+        _     -> False
+  where
+    colisaoPlataforma coord =
+        case obterValor mapa coord of
+            Plataforma -> True
+            Alcapao    -> True
+            Vazio      -> False
+            Escada     -> False
 
-colisoesChao :: Mapa -> Personagem -> Bool
-colisoesChao (Mapa _ _ mapa) Personagem {posicao = (x,y)} =
-         colisaoPlataforma (round (x/16),round ((y+16)/16)) || colisaoPlataforma (round (x/16),round ((y-16)/16))
-    where 
-        colisaoPlataforma coord = 
-            case obterValor mapa coord of
-                Plataforma -> True 
-                Alcapao -> True
-                Vazio -> False
-                Escada -> False
+-- | Verifica colisões com o chão.
+colisoesChao :: Mapa          -- ^ Mapa atual do jogo.
+             -> Personagem    -- ^ Personagem do jogador.
+             -> Bool          -- ^ Verdadeiro se houver colisão, Falso caso contrário.
+colisoesChao (Mapa _ _ mapa) Personagem {posicao = (x, y)} =
+    colisaoPlataforma (round (x / 16), round ((y + 16) / 16)) || colisaoPlataforma (round (x / 16), round ((y - 16) / 16))
+  where
+    colisaoPlataforma coord =
+        case obterValor mapa coord of
+            Plataforma -> True
+            Alcapao    -> True
+            Vazio      -> False
+            Escada     -> False
 
-obterValor :: [[Bloco]]-> (Int,Int) -> Bloco
-obterValor (h:t) (x,y)        | y' == 0      = obterColuna h (x,y)
-                              | otherwise    = obterValor t (x,y'-1)
-        where y' = abs y
+-- | Obtém o valor em uma posição específica no mapa do jogo.
+obterValor :: [[Bloco]]  -- ^ Lista 2D representando o mapa do jogo.
+           -> (Int, Int) -- ^ Coordenadas da posição (coluna, linha).
+           -> Bloco      -- ^ Valor na posição especificada.
+obterValor (h : t) (x, y)
+    | y' == 0 = obterColuna h (x, y)
+    | otherwise = obterValor t (x, y' - 1)
+  where
+    y' = abs y
 
-obterColuna :: [Bloco] -> (Int,Int) -> Bloco
-obterColuna (h:t) (x,y) |x == 0     = h
-                        |otherwise  = obterColuna t (x-1 , y)
+-- | Obtém o valor em uma coluna específica no mapa do jogo.
+obterColuna :: [Bloco]    -- ^ Lista representando uma coluna no mapa do jogo.
+            -> (Int, Int) -- ^ Coordenadas da posição (coluna, linha).
+            -> Bloco      -- ^ Valor na coluna especificada.
+obterColuna (h : t) (x, y)
+    | x == 0 = h
+    | otherwise = obterColuna t (x - 1, y)
 
-colisoesPersonagem :: Personagem -> Personagem -> Bool
-colisoesPersonagem Personagem {tamanho = (a1,b1), posicao = (x1,y1)} Personagem {tamanho = (a2,b2), posicao = (x2,y2)} = 
-        not (x1 + (a1/2) < x2 - (a2/2)|| x2 + (a2/2) < x1 - (a1/2) || y1 + (b1/2) < y2 - (b2/2)|| y2 + (b2/2) < y1 - (a1/2))
-
-
-
-
+-- | Verifica colisões entre dois personagens.
+colisoesPersonagem :: Personagem -- ^ Primeiro personagem.
+                  -> Personagem  -- ^ Segundo personagem.
+                  -> Bool        -- ^ Verdadeiro se houver colisão, Falso caso contrário.
+colisoesPersonagem Personagem {tamanho = (a1, b1), posicao = (x1, y1)} Personagem {tamanho = (a2, b2), posicao = (x2, y2)} =
+    not (x1 + (a1 / 2) < x2 - (a2 / 2) || x2 + (a2 / 2) < x1 - (a1 / 2) || y1 + (b1 / 2) < y2 - (b2 / 2) || y2 + (b2 / 2) < y1 - (a1 / 2))
