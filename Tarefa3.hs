2a72dc665d10fbbc658b78e5a6b90b8c84c25169
Hugo Soares <a107293@alunos.uminho.pt>
2024-01-16 16:41:55 +0000

    Changed commented stuff

--- Tarefa3.hs
+++ Tarefa3.hs
@@ -9,12 +9,14 @@ movimenta :: Semente -> Tempo -> Jogo -> Jogo
 A função movimenta deve animar todos os personagens, i.e. calcular as
 suas novas posições e respectivas consequências. 
 -}
+
 module Tarefa3 where
 
 import DataStruct
 import Tarefa1
 import Maps
 
+-- | Função principal que movimenta os personagens no jogo.
 movimenta :: Semente -> Tempo -> Jogo -> Jogo
 movimenta semente tempo jogo = 
     jogoColide
@@ -23,58 +25,66 @@ movimenta semente tempo jogo =
             pegarColec = pegarcolecionaveis jogoGravidade
             jogoColide = bater pegarColec
 
-
+-- | Função que lida com as colisões entre personagens e colecionáveis.
 bater :: Jogo -> Jogo
 bater jogo@Jogo {jogador = mario@Personagem {aplicaDano = (danoX, _), vida = v}, inimigos = (ini:resto)} = 
-          case (colisaohitbox mario ini,colisoesPersonagem mario ini ) of
-            (True,_) -> bater jogo {inimigos = resto ++ [ini {vida = 0, aplicaDano = (False, 0)}]}
-            (_,True) -> jogo {jogador = mario {vida = v-1}}
+          case (colisaohitbox mario ini, colisoesPersonagem mario ini) of
+            (True, _) -> bater jogo {inimigos = resto ++ [ini {vida = 0, aplicaDano = (False, 0)}]}
+            (_, True) -> jogo {jogador = mario {vida = v-1}}
             _ -> jogo
 
+-- | Verifica colisões entre as hitboxes dos personagens.
 colisaohitbox :: Personagem -> Personagem -> Bool
-colisaohitbox p1@Personagem {tipo = Jogador, aplicaDano = (True,g), direcao = dir, posicao = (x,y), tamanho = (larg,alt )}
-      p2@Personagem {posicao = (x1,y1), tamanho = t} = 
+colisaohitbox p1@Personagem {tipo = Jogador, aplicaDano = (True, g), direcao = dir, posicao = (x, y), tamanho = (larg, alt)}
+      p2@Personagem {posicao = (x1, y1), tamanho = t} = 
           case dir of 
-            Leste -> colisoesPersonagem p1{posicao = (x+larg,y)} p2
-            Oeste -> colisoesPersonagem p1{posicao = (x-larg,y)} p2
+            Leste -> colisoesPersonagem p1{posicao = (x+larg, y)} p2
+            Oeste -> colisoesPersonagem p1{posicao = (x-larg, y)} p2
             _ -> False
 colisaohitbox _ _ = False
 
+-- | Aplica a gravidade ao jogador.
 aplicaGravidade :: Jogo -> Jogo
 aplicaGravidade jogo@(Jogo menu mapa mal colec jog) =
     jogo { jogador = aplicarGravidadeAoPersonagem mapa jog }
 
+-- | Aplica a gravidade a um personagem.
 aplicarGravidadeAoPersonagem :: Mapa -> Personagem -> Personagem
 aplicarGravidadeAoPersonagem mapa p@(Personagem vel tipo (x, y) dir tam emEsc res vida pontos dano velVert) =
     p { posicao = (x, y + velVert), velocidadeVertical = novaVelocidadeVert }
   where
     novaVelocidadeVert = if colisoesParede mapa p Sul then 0 else velVert + gravidade
 
+-- | Atualiza o jogo ao pegar colecionáveis.
 pegarcolecionaveis :: Jogo -> Jogo
 pegarcolecionaveis jogo@Jogo {colecionaveis = colec, jogador = mario} =
       if checaProximo jogo then
         let (jogoAtualizado, personagemAtualizado) = atualizarEstadoJogador jogo
         in jogoAtualizado {jogador = personagemAtualizado, colecionaveis = filter (naocolide personagemAtualizado) colec}
       else jogo
-    where naocolide Personagem {posicao = (x,y), tamanho = (larg,alt)} (_,pItem) = 
+    where naocolide Personagem {posicao = (x, y), tamanho = (larg, alt)} (_, pItem) = 
               procuraMatriz l pItem /= procuraMatriz l (x-(larg/2), y) &&
               procuraMatriz l pItem /= procuraMatriz l (x+(larg/2),y) 
 
-procuraMatriz :: TBloco -> Posicao -> (Int,Int)
-procuraMatriz l (x,y) = (round (x/16), round (y/16))
+-- | Procura a posição de um ponto na matriz do mapa.
+procuraMatriz :: TBloco -> Posicao -> (Int, Int)
+procuraMatriz l (x, y) = (round (x/16), round (y/16))
 
+-- | Verifica se o jogador está próximo a algum colecionável.
 checaProximo :: Jogo -> Bool
-checaProximo Jogo {jogador = Personagem {posicao = (x,y), aplicaDano = (d,tempo), tamanho = (larg,alt)}, colecionaveis = colec} =
-      any (\(_,pItem) -> procuraMatriz l pItem == procuraMatriz l (x-(larg/2), y) || procuraMatriz l pItem == procuraMatriz l (x+(larg/2),y)) colec
+checaProximo Jogo {jogador = Personagem {posicao = (x, y), aplicaDano = (d, tempo), tamanho = (larg, alt)}, colecionaveis = colec} =
+      any (\(_, pItem) -> procuraMatriz l pItem == procuraMatriz l (x-(larg/2), y) || procuraMatriz l pItem == procuraMatriz l (x+(larg/2),y)) colec
 
+-- | Atualiza o estado do jogador ao pegar um colecionável.
 atualizarEstadoJogador :: Jogo -> (Jogo, Personagem)
-atualizarEstadoJogador jogo@Jogo {colecionaveis = ((colec,pItem):t), jogador = p@(Personagem {tipo = Jogador, pontos = ponto, posicao = posicaoPersonagem, tamanho = (larg,alt)})} = 
+atualizarEstadoJogador jogo@Jogo {colecionaveis = ((colec, pItem):t), jogador = p@(Personagem {tipo = Jogador, pontos = ponto, posicao = posicaoPersonagem, tamanho = (larg, alt)})} = 
     (jogoAtualizado, personagemAtualizado)
     where (jogoAtualizado, personagemAtualizado) = case colec of 
             Moeda -> (jogo { colecionaveis = t }, p {pontos = ponto+200})
             Martelo -> (jogo { colecionaveis = t, jogador = pAtualizado}, pAtualizado)
               where pAtualizado =  p { aplicaDano = (True, 10), pontos = ponto}
-      
+
+-- | Movimenta os inimigos do jogo.
 movimentaInimigo :: Semente -> Tempo -> Personagem -> Personagem
 movimentaInimigo semente tempo ini@Personagem {direcao = dire, velocidade = v}= 
             case dire of
@@ -83,5 +93,6 @@ movimentaInimigo semente tempo ini@Personagem {direcao = dire, velocidade = v}=
               Oeste -> if colisoesParede mapaGrande ini Oeste then moverInimigo ini {direcao = Leste, velocidade = -v}
                                                               else moverInimigo ini
 
+-- | Move um inimigo na direção especificada.
 moverInimigo :: Personagem -> Personagem 
 moverInimigo ini@Personagem {velocidade = v, posicao = (x,y)} = ini {posicao = (x-v,y)}
--- Tarefa4.hs
+++ Tarefa4.hs
@@ -1,15 +1,13 @@
 {-|
-Module : Tarefa 4
-Description : Modulo Haskell Gloss Tarefa4
+Module: Tarefa4
+Description: Modulo Haskell Gloss Tarefa4
 Copyright : Hugo Soares (a107293)
             Henrique Brito (a107378)
-Actualização das velocidades e direcções dos
-personagens
-O objectivo desta tarefa é implementar a função
-atualiza :: [Maybe Acao] -> Maybe Acao -> Jogo -> Jogo
-A função atualiza deve validar e calcular as novas direções e velocidades
+O objetivo desta tarefa é implementar a função 'atualiza'
+que valida e calcula as novas direções e velocidades
 dos personagens (inimigos e jogador) de acordo com as ações dadas.
 -}
+
 module Tarefa4 where
 
 import Graphics.Gloss
@@ -19,19 +17,22 @@ import Maps
 import Data.List
 import Tarefa1
 
-
+-- | Função principal que atualiza o estado do jogo com base nas ações fornecidas.
 atualiza :: [Maybe Acao] -> Acao -> Jogo -> Jogo
 atualiza acao1 acaop jogo = jogo
 
+-- | Função para reagir a eventos de teclado, atualizando a lista de teclas pressionadas.
 reactKey :: Event -> [Key] -> [Key]
 reactKey (EventKey key Up _ _)   = delete key
 reactKey (EventKey key Down _ _) = (:) key
 reactKey _                      = id
 
+-- | Aplica a lista de teclas pressionadas ao jogo.
 aplicaListaKey :: [Key] -> Jogo -> Jogo
 aplicaListaKey [] j          = j
 aplicaListaKey (h:t) e = aplicaListaKey t (aplicaKey h e)
 
+-- | Aplica uma tecla específica ao jogo.
 aplicaKey :: Key -> Jogo -> Jogo
 aplicaKey (SpecialKey KeyUp) j@(Jogo _ _ ini _ p)        = if colisoesParede (mapa j) p Norte || colisoesPersonagens p ini then j else jogada (Just Subir) j
 aplicaKey (SpecialKey KeyDown) j@(Jogo _ _ ini _ p)      = if colisoesParede (mapa j) p Sul || colisoesPersonagens p ini then j else jogada (Just Descer) j
@@ -39,28 +40,31 @@ aplicaKey (SpecialKey KeyLeft) j@(Jogo _ _ ini _ p)      = if colisoesParede (ma
 aplicaKey (SpecialKey KeyRight) j@(Jogo _ _ ini _ p)     = if colisoesParede (mapa j) p Leste || colisoesPersonagens p ini  then j else jogada (Just AndarDireita) j
 aplicaKey _ j                                            = j
 
-
-jogada ::Maybe Acao -- A 'Jogada' a efetuar.
-        -> Jogo -- O 'Estado' anterior.
-        -> Jogo -- O 'Estado' resultante após o jogador efetuar a jogada.      
+-- | Realiza a jogada com base na ação fornecida.
+jogada :: Maybe Acao -- ^ A 'Jogada' a efetuar.
+        -> Jogo -- ^ O 'Estado' anterior.
+        -> Jogo -- ^ O 'Estado' resultante após o jogador efetuar a jogada.
 jogada a (Jogo menu m ini colec jo) = Jogo menu m ini colec (playerNovo jo)
-                                               where
-                                                     playerNovo jo = alteraJogada jo a (Jogo menu m ini colec jo)
+  where
+    playerNovo jo = alteraJogada jo a (Jogo menu m ini colec jo)
 
+-- | Altera a jogada com base na ação fornecida.
 alteraJogada :: Personagem -> Maybe Acao -> Jogo -> Personagem
-alteraJogada p@(Personagem {posicao = (x,y), velocidade = velo}) a e |a == Just Saltar              = p {posicao =(x,y+jump)}
-                                                                     |a == Just Subir               = p {posicao =(x,y+velo)}
-                                                                     |a == Just Descer              = p {posicao =(x,y-velo)}
-                                                                     |a == Just AndarEsquerda       = p {posicao =(x-velo,y),direcao = Oeste}
-                                                                     |a == Just AndarDireita        = p {posicao =(x+velo,y),direcao = Leste}
+alteraJogada p@(Personagem {posicao = (x, y), velocidade = velo}) a e
+  | a == Just Saltar              = p {posicao = (x, y + jump)}
+  | a == Just Subir               = p {posicao = (x, y + velo)}
+  | a == Just Descer              = p {posicao = (x, y - velo)}
+  | a == Just AndarEsquerda       = p {posicao = (x - velo, y), direcao = Oeste}
+  | a == Just AndarDireita        = p {posicao = (x + velo, y), direcao = Leste}
 
+-- | Verifica a colisão com uma escada no mapa.
 colisaoEscada :: Mapa -> Personagem -> Bool
-colisaoEscada map@(Mapa a b mapa) p@Personagem {posicao = (x,y)} = 
-                    case (obterValor mapa (round (x/16),round (y+16/16)),obterValor mapa (round (x/16),round (y/16)),obterValor mapa (round (x/16),round (y-16/16))) of
-                            (Escada,Escada,Escada) -> True
-                            (Plataforma,Escada,Escada) -> True
-                            (Escada,Plataforma,_) -> True
-                            (_,Escada,_) -> True
-                            (Escada,Vazio,_) -> True
-                            (_,_,Escada) -> True
-                            (_,_,_) -> False
+colisaoEscada map@(Mapa a b mapa) p@(Personagem {posicao = (x, y)}) =
+  case (obterValor mapa (round (x / 16), round (y + 16 / 16)), obterValor mapa (round (x / 16), round (y / 16)), obterValor mapa (round (x / 16), round (y - 16 / 16))) of
+    (Escada, Escada, Escada) -> True
+    (Plataforma, Escada, Escada) -> True
+    (Escada, Plataforma, _) -> True
+    (_, Escada, _) -> True
+    (Escada, Vazio, _) -> True
+    (_, _, Escada) -> True
+    (_, _, _) -> False
